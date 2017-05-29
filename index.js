790db2a02e849d4c5ab2b07708e243d624248863
Dmitriy Kolesnikov <xxsnakerxx@gmail.com>
2017-05-30 01:19:21 +0600

    Made active prop as controlled

--- index.js
+++ index.js
@@ -87,6 +87,16 @@ class Switch extends React.Component {
     });
   };
 
+  componentWillReceiveProps = (nextProps) => {
+    if (this.state.state !== nextProps.active) {
+      if (nextProps.active) {
+        this.activate();
+      } else {
+        this.deactivate();
+      }
+    }
+  }
+
   onSwipe = (currentPosition, startingPosition, onChange, onTerminate) => {
     if (currentPosition - startingPosition >= 0) {
       if (currentPosition - startingPosition > this.state.width / 2 || startingPosition == this.state.width) {
